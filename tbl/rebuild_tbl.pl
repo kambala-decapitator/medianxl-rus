f269673101e9914ebd1ef7eb10728e861b916081
Andrey Filipenkov <decapitator@ukr.net>
2017-12-31 19:14:41 +0300

    update main translation

--- tbl/rebuild_tbl.pl
+++ tbl/rebuild_tbl.pl
@@ -6,13 +6,14 @@ use File::Path qw/make_path/;
 
 my $tblDir = $ARGV[0] or die 'tbl path not specified';
 my @tbls = qw/string expansionstring patchstring/;
+my $tblLineRegex = qr/^(.+?)\t(.+)$/;
 
 # ru tbls -> hash
 my %ruStrings;
 for my $tbl (@tbls) {
     open my $tblHandle, "<", "$tblDir/ru/$tbl.txt";
     for (<$tblHandle>) {
-        $ruStrings{$1} = $2 if (/^(.+?)\t(.+)$/)
+        $ruStrings{$1} = $2 if /$tblLineRegex/
     }
     close $tblHandle
 }
@@ -25,9 +26,9 @@ for my $tbl (@tbls) {
     open my $tblHandle, "<", "$tblDir/en/$tbl.txt";
     open my $ruOutTblHandle, ">", "$ruOutDir/$tbl.txt";
     for (<$tblHandle>) {
-        next unless (/^(.+?)\t(.+)$/);
+        next unless /$tblLineRegex/;
         my ($key, $str) = ($1, $2);
-        print $ruOutTblHandle $1, "\t", $ruStrings{$1} // $2, "\n";
+        print $ruOutTblHandle $key, "\t", $ruStrings{$key} // $str, "\n";
     }
     close $tblHandle
 }
